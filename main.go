a0389ffa5288fa3881390d7ebcb2258bb42795d3
iselink <33873947+iselink@users.noreply.github.com>
2022-11-26 18:12:52 +0100

    Rapid development 2x

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"flag"
 	"io/ioutil"
-	"log"
-	"net"
 	"os"
-	"sync"
 )
 
 const module_root = "/sys/module/hid_apple/parameters"
@@ -14,88 +11,57 @@ const fnParam = "/sys/module/hid_apple/parameters/fnmode"
 const isoLayoutParam = "/sys/module/hid_apple/parameters/iso_layout"
 const swapFnLeftCtrlParam = "/sys/module/hid_apple/parameters/swap_fn_leftctrl"
 const swapOptionCommandParam = "/sys/module/hid_apple/parameters/swap_opt_cmd"
-const sockPath = "/tmp/hidDaemon.sock"
 
 func main() {
-	daemon := flag.Bool("daemon", false, "Run progam as daemon")
-	daemonFrontend := flag.Bool("daemon-ui", false, "Run progam as daemon in tray (show nice clickable checkboxes).") //TODO: make UI frontend
+	//daemon := flag.Bool("daemon", false, "Run progam as daemon")
+	//daemonFrontend := flag.Bool("daemon-ui", false, "Run progam as daemon in tray (show nice clickable checkboxes).") //TODO: make UI frontend
 	fnKeyMode := flag.Int("fn", -1, "Set 1 to enable or 0 to disable fn keys.")
 	isoLayoutMode := flag.Int("iso", -1, "Set 1 to enable or 0 to disable ISO layout.")
-	swapFnLeftCtrlMode := flag.Int("fn", -1, "Set 1 to enable or 0 to disable swapping Fn and left Ctrl.")
-	swapOptCmdMode := flag.Int("fn", -1, "Set 1 to enable or 0 to disable swapping Opt and Cmd keys..")
+	swapFnLeftCtrlMode := flag.Int("swap-fn-lctrl", -1, "Set 1 to enable or 0 to disable swapping Fn and left Ctrl.")
+	swapOptCmdMode := flag.Int("swap-opt-cmd", -1, "Set 1 to enable or 0 to disable swapping Opt and Cmd keys..")
 
 	flag.Parse()
 
-	if *daemon {
-		runAsDaemon()
-	} else if *daemonFrontend {
-		runAsUiFrontend()
-	} else {
-		setValues(fnKeyMode, isoLayoutMode, swapFnLeftCtrlMode, swapOptCmdMode)
-	}
+	setValues(fnKeyMode, isoLayoutMode, swapFnLeftCtrlMode, swapOptCmdMode)
 
 }
 
 func setValues(fnKeyMode *int, isoLayoutMode *int, swapFnLeftCtrlMode *int, swapOptCmdMode *int) {
-	switch *fnKeyMode {
-	case 0:
-		{
-			key, err := setFnKey(false)
-			if err != nil {
-				println(err.Error())
-			}
-			println(key)
-			break
-		}
-	case 1:
-		{
-			key, err := setFnKey(true)
-			if err != nil {
-				println(err.Error())
-			}
-			println(key)
-			break
-		}
-
-	}
-
-	//TODO: complete func with all parameters
-}
-
-func runAsUiFrontend() {
-	wait := sync.WaitGroup{}
-	wait.Add(1)
-
-	wait.Wait()
-
-}
-
-func runAsDaemon() {
-	if err := os.Remove(sockPath); err != nil {
-		log.Fatal(err)
-	}
-
-	l, err := net.Listen("unix", sockPath)
-	if err != nil {
-		log.Fatal("listen error:", err)
-	}
-	defer l.Close()
-
-	for {
-		// Accept new connections, dispatching them to echoServer
-		// in a goroutine.
-		conn, err := l.Accept()
+	if *fnKeyMode != -1 {
+		key, err := writeParam(fnParam, !(*fnKeyMode == 0))
 		if err != nil {
-			log.Fatal("accept error:", err)
-		} else {
-			go func(con net.Conn) {
-				//TODO: complete
-			}(conn)
+			println(err.Error())
 		}
+		println(key)
 	}
+
+	if *isoLayoutMode != -1 {
+		key, err := writeParam(isoLayoutParam, !(*isoLayoutMode == 0))
+		if err != nil {
+			println(err.Error())
+		}
+		println(key)
+	}
+
+	if *swapFnLeftCtrlMode != -1 {
+		key, err := writeParam(swapFnLeftCtrlParam, !(*swapFnLeftCtrlMode == 0))
+		if err != nil {
+			println(err.Error())
+		}
+		println(key)
+	}
+
+	if *swapOptCmdMode != -1 {
+		key, err := writeParam(swapOptionCommandParam, !(*swapOptCmdMode == 0))
+		if err != nil {
+			println(err.Error())
+		}
+		println(key)
+	}
+
 }
 
-func setFnKey(value bool) (bool, error) {
+func writeParam(path string, value bool) (bool, error) {
 	var data []byte
 	if value {
 		data = []byte("1")
@@ -104,16 +70,18 @@ func setFnKey(value bool) (bool, error) {
 	}
 
 	//get perms
-	lstat, err := os.Lstat(fnParam)
+	lstat, err := os.Lstat(path)
 	if err != nil {
 		return false, err
 	}
 
 	//write
-	err = ioutil.WriteFile(fnParam, data, lstat.Mode())
+	err = ioutil.WriteFile(path, data, lstat.Mode())
 	if err != nil {
 		return false, err
 	}
+
+	println(path, "=", string(data))
 
 	return true, nil
 }
