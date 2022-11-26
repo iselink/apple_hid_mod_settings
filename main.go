6528b852cacd160fa97b1758939e7bb36cc0565e
iselink <33873947+iselink@users.noreply.github.com>
2022-11-26 17:55:03 +0100

    Rapid development

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"io/ioutil"
+	"log"
+	"net"
 	"os"
+	"sync"
 )
 
 const module_root = "/sys/module/hid_apple/parameters"
@@ -11,18 +14,29 @@ const fnParam = "/sys/module/hid_apple/parameters/fnmode"
 const isoLayoutParam = "/sys/module/hid_apple/parameters/iso_layout"
 const swapFnLeftCtrlParam = "/sys/module/hid_apple/parameters/swap_fn_leftctrl"
 const swapOptionCommandParam = "/sys/module/hid_apple/parameters/swap_opt_cmd"
+const sockPath = "/tmp/hidDaemon.sock"
 
 func main() {
 	daemon := flag.Bool("daemon", false, "Run progam as daemon")
+	daemonFrontend := flag.Bool("daemon-ui", false, "Run progam as daemon in tray (show nice clickable checkboxes).") //TODO: make UI frontend
 	fnKeyMode := flag.Int("fn", -1, "Set 1 to enable or 0 to disable fn keys.")
+	isoLayoutMode := flag.Int("iso", -1, "Set 1 to enable or 0 to disable ISO layout.")
+	swapFnLeftCtrlMode := flag.Int("fn", -1, "Set 1 to enable or 0 to disable swapping Fn and left Ctrl.")
+	swapOptCmdMode := flag.Int("fn", -1, "Set 1 to enable or 0 to disable swapping Opt and Cmd keys..")
 
 	flag.Parse()
 
 	if *daemon {
 		runAsDaemon()
-		return
+	} else if *daemonFrontend {
+		runAsUiFrontend()
+	} else {
+		setValues(fnKeyMode, isoLayoutMode, swapFnLeftCtrlMode, swapOptCmdMode)
 	}
 
+}
+
+func setValues(fnKeyMode *int, isoLayoutMode *int, swapFnLeftCtrlMode *int, swapOptCmdMode *int) {
 	switch *fnKeyMode {
 	case 0:
 		{
@@ -44,10 +58,41 @@ func main() {
 		}
 
 	}
+
+	//TODO: complete func with all parameters
+}
+
+func runAsUiFrontend() {
+	wait := sync.WaitGroup{}
+	wait.Add(1)
+
+	wait.Wait()
+
 }
 
 func runAsDaemon() {
+	if err := os.Remove(sockPath); err != nil {
+		log.Fatal(err)
+	}
 
+	l, err := net.Listen("unix", sockPath)
+	if err != nil {
+		log.Fatal("listen error:", err)
+	}
+	defer l.Close()
+
+	for {
+		// Accept new connections, dispatching them to echoServer
+		// in a goroutine.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("accept error:", err)
+		} else {
+			go func(con net.Conn) {
+				//TODO: complete
+			}(conn)
+		}
+	}
 }
 
 func setFnKey(value bool) (bool, error) {
